9ba828ee5f7b2d78a492301731b06425f737a59b
Elvis <elvis992035@gmail.com>
2018-11-28 17:06:43 -0500

    server works

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const path = 3000;
+const path = 4000;
 
-app.listen(path,() => console.log("server is running in port 3000"));
+app.listen(path,() => console.log("server is running in port 4000"));
