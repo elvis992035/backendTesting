4e7f90fc0c290c672443830029f60e761fa0a559
elvis992035@gmail.com <elvis992035@gmail.com>
2018-11-28 23:56:47 -0500

    mongoDB connect

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,30 @@
+//  First - create the server in port 4000 with express
+
 const express = require('express');
-
 const app = express();
-
 const path = 4000;
 
 app.listen(path,() => console.log("server is running in port 4000"));
+
+//  Second - connect to the mongoDB
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+
+    'mongodb+srv://elvis992035:FLCWKJqJy6BtH8Su@cluster0-wvkpb.mongodb.net/test?retryWrites=true', 
+
+    { useNewUrlParser: true }
+
+)
+.then(() => {
+    console.log('connected to database');
+})
+.catch(err => {
+    console.log('connection failed', err);
+});
+
+mongoose.set('useCreateIndex', true);
+
+// const userRouter = require('/routing/user.js');
+// const dataRouter = require('./routing/data');
