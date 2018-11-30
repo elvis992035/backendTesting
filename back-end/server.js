9c65ad120e9f4ca4773b023c532ea8738740539c
Elvis <elvis992035@gmail.com>
2018-11-30 16:23:43 -0500

    get and post are done

--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,24 @@
-//  First - create the server in port 4000 with express
-
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const path = 4000;
 
-app.listen(path,() => console.log("server is running in port 4000"));
+// importing routers
 
-//  Second - connect to the mongoDB
+// const userRouter = require('/routing/user.js');
+const dataRouter = require('./routing/data');
 
-const mongoose = require('mongoose');
+//  middleware
+
+app.use(express.json());
+// app.use(cors());
 
 mongoose.connect(
 
-    'mongodb+srv://elvis992035:FLCWKJqJy6BtH8Su@cluster0-wvkpb.mongodb.net/test?retryWrites=true', 
+  // 'mongodb+srv://shal1231:Shal1231@cluster0-wlagw.mongodb.net/test?retryWrites=true',
+
+  'mongodb+srv://elvis992035:FLCWKJqJy6BtH8Su@cluster0-wvkpb.mongodb.net/test?retryWrites=true',
 
     { useNewUrlParser: true }
 
@@ -24,7 +30,16 @@ mongoose.connect(
     console.log('connection failed', err);
 });
 
+// to get rid of this error
+// (node:66003) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+
 mongoose.set('useCreateIndex', true);
 
-// const userRouter = require('/routing/user.js');
-// const dataRouter = require('./routing/data');
+// router
+
+ app.use('/', dataRouter);
+
+ //  routing it in port 4000
+
+ app.listen(path,() => console.log("server is running in port 4000"));
+
